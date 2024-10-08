234ec5113c464c97bbec72ffc8924fc0b215ddd8
LawrenceDong71 <138734563+LawrenceDong71@users.noreply.github.com>
2024-10-08 17:28:33 -0500

    Update NameSurfer.java

--- September 19th/NameSurfer.java
+++ September 19th/NameSurfer.java
@@ -1,17 +1,3 @@
-/*
- * Student information for assignment: Replace <NAME> in the following with your
- * name. You are stating, on your honor you did not copy any other code on this
- * assignment and have not provided your code to anyone.
- *
- * On my honor, Lawrence Dong, this programming assignment is my own work
- * and I have not provided this code
- * to any other student.
- *
- * UTEID: lrd2265
- * email address: lawrencerdong@gmail.com
- * Number of slip days I am using: 0
- */
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
